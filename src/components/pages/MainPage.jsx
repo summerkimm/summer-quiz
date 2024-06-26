3dc6661aecf6ed16abf7bb0e505395146d11fc58
Soeun Kim <summerkim0825@gmail.com>
2024-06-26 18:37:47 +0900

    Refactor: 메인 페이지 CSS 수정

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,16 +1,11 @@
 import { Link } from "react-router-dom";
 import Slide from "../ui/Slide";
 import "./MainPage.css";
-import { useState } from 'react';
 
 function MainPage() {
-  // const [animate, setAnimate] = useState(true);
-  // const onStop = () => setAnimate(false);
-  // const onRun = () => setAnimate(true);
-
   return (
     <div className="main">
-      <p>
+      <p className='main-text'>
         당신은 <span className="name">김소은</span>에 대해 얼마나 알고 있나요?
       </p>
       <Slide />
