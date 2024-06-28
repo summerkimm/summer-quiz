581b336831e3d641659d65a0873a62b24d82e520
Soeun Kim <summerkim0825@gmail.com>
2024-06-28 16:53:47 +0900

    Feat: 전체 box-shadow 추가

--- src/components/CommonLayout.jsx
+++ src/components/CommonLayout.jsx
@@ -1,7 +1,14 @@
 function CommonLayout({ children }) {
   return (
     <>
-      <h1 style={{ fontSize: "36px" }}>김소은 테스트</h1>
+      <h1
+        style={{
+          fontSize: "36px",
+          textShadow: "10px 10px 10px rgba(0, 0, 0, 0.15)",
+        }}
+      >
+        김소은 테스트
+      </h1>
       <div className="question-section">{children}</div>
     </>
   );
